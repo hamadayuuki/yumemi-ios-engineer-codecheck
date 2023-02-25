c980bb1c0bb304b0936aaa31c861a1d48eb4c030
hamadayuuki <yuukirinnma@gmail.com>
2023-02-26 03:12:28 +0900

    update: Webページ表示の待ち時間にインジケーター表示

--- iOSEngineerCodeCheck/View/WebUIViewController.swift
+++ iOSEngineerCodeCheck/View/WebUIViewController.swift
@@ -15,12 +15,14 @@ class WebUIViewContorller: UIViewController {
     var url: String!
     var barTitle: String!
     var uiScrollView = UIScrollView()
+    var activityIndicatorView: UIActivityIndicatorView!
 
     init(url: String, barTitle: String) {
         super.init(nibName: nil, bundle: nil)
 
         self.url = url
         self.barTitle = barTitle
+        self.activityIndicatorView = UIActivityIndicatorView()
     }
 
     override func viewDidLoad() {
@@ -40,12 +42,19 @@ class WebUIViewContorller: UIViewController {
 
     func setupLayout() {
         let webView = WKWebView(frame: view.frame)
+        webView.navigationDelegate = self
         let request = URLRequest(url: URL(string: self.url)!)
         view.addSubview(webView)
         webView.load(request)
 
         view.addSubview(webView)
+
+        self.activityIndicatorView.style = .large
+        self.activityIndicatorView.layer.position = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+        view.addSubview(self.activityIndicatorView)
     }
+
+    // MARK: Button Action
 
     @objc func tapDismissButton(_ sender: UIBarButtonItem) {
         self.dismiss(animated: true, completion: nil)
@@ -53,5 +62,25 @@ class WebUIViewContorller: UIViewController {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - WKNavigationDelegate
+
+extension WebUIViewContorller: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        self.activityIndicatorView.startAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        self.activityIndicatorView.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        self.activityIndicatorView.stopAnimating()
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        self.activityIndicatorView.stopAnimating()
     }
 }
