e551ca17b7c71998e8483e6d1f80d937b13c4ea7
hamadayuuki <yuukirinnma@gmail.com>
2023-02-27 01:13:55 +0900

    update: リポジトリ一覧へリポジトリを追加可能に

--- iOSEngineerCodeCheck/ViewModel/RepositoriesTableViewModel.swift
+++ iOSEngineerCodeCheck/ViewModel/RepositoriesTableViewModel.swift
@@ -34,4 +34,24 @@ class RepositoriesTableViewModel: ObservableObject {
             self.isLoading = false
         }
     }
+
+    public func addRepositories(searchText: String, page: Int) async throws {
+        guard !isLoading else { return }  // 連続でAPIを叩かないように
+        let githubAPISearchUrl = "https://api.github.com/search/repositories?q=\(searchText)&page=\(2)"
+        isLoading = true
+        do {
+            let result = try await self.request.fetchData(url: githubAPISearchUrl)
+            self.isLoading = false
+            switch result {
+            case let .success(data):
+                let repos: Repositories = try! JSONDecoder().decode(Repositories.self, from: data)  // TODO: 変数名変更, Repositoriesの命名から変更
+                self.repositories += repos.items
+            case let .failure(apiError):
+                self.apiErrorAlart = APIErrorAlart(title: apiError.title, description: apiError.description)
+            }
+        } catch (let error) {
+            print(error.localizedDescription)
+            self.isLoading = false
+        }
+    }
 }
